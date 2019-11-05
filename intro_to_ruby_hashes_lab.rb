138cf9fa1132a1c549eee3dd8a319ebba5cfa742
Jeremy Perry <jeremy.m.perry@gmail.com>
2019-11-05 21:58:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -47,7 +47,7 @@ def monopoly_with_fourth_tier
       },
       :names=>{
         :reading_railroad=>{
-          ""
+          "M"
         },
         :pennsylvania_railroad=>{},
         :b_and_o_railroad=>{},
